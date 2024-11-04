db06d6b8c5a1baece39f92c80ee8bb9f49943380
ankitahlwat <ankitahlwat54@gmail.com>
2024-11-03 22:11:33 -0800

    Add route for getting symbol price

--- server/server.js
+++ server/server.js
@@ -14,9 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET;
 
-app.use(cors({ 
-    origin: ['https://isa-project-client.netlify.app', 'https://www.isa-project-client.netlify.app'], 
-    credentials: true 
+app.use(cors({
+    origin: ['https://isa-project-client.netlify.app', 'https://www.isa-project-client.netlify.app', 'http://127.0.0.1:5500'],
+    credentials: true
 }));
 app.use(bodyParser.json());
 
