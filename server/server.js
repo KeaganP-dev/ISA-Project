777b0b47a07ffb8e8c02a3746b1ce8b252dabc24
Keagan <keaganpurtell@hotmail.com>
2024-11-03 21:42:58 -0800

    added www url and removed extra /

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 app.use(cors({ 
-    origin: 'https://isa-project-client.netlify.app/', 
+    origin: ['https://isa-project-client.netlify.app', 'https://www.isa-project-client.netlify.app'], 
     credentials: true 
 }));
 app.use(bodyParser.json());
